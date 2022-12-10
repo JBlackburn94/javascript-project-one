ba10b6b747c9d6cfe89b3bad2bb6c7339dd33477
Jason Blackburn <blackburnjason1994@gmail.com>
2022-12-10 23:40:33 +0000

    Created the first event listeners to make the buttons functional and also wrote the function to provide the user with questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,25 @@
-function runGame(){
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName('button');
 
+    for(let button of buttons){
+        button.addEventListener("click", function(){
+            if(this.getAttribute("data-type") === "submit"){
+                alert("You clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+});
+
+/** This function is used to load a question when 
+ * the game is first loaded and again following an
+ * initial answer. 
+ */
+function runGame(){
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer(){
